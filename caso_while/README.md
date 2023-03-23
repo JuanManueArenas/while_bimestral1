9fb6c24430768047027078ba1adccfdc543b2791
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-03-23 18:37:54 -0500

    se agrego el programa y su diagrama al README

---
+++ caso_while/README.md
@@ -0,0 +1,7 @@
+# programa para hallar los primeros N numeros naturales
+
+# Diseño 
+
+## Diagrama de flujo 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ caso_while/caso1.py
@@ -0,0 +1,13 @@
+N = int(input("agrege el valor de N: "))
+suma=0
+i= 1
+
+while(i<=N):
+    suma = suma + i
+    i = i + 1
+
+print("------------------------------")
+print("-----La suma es igual a ------")
+print("------------------------------")
+print(suma)
+    
